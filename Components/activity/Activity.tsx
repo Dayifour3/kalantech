2fbb1b5cc44bcdb28168be0b5c2b490902d9e886
Dayifour3 <123929396+Dayifour3@users.noreply.github.com>
2025-01-01 02:22:26 +0000

    ajout de composant footer

--- Components/activity/Activity.tsx
+++ Components/activity/Activity.tsx
@@ -2,7 +2,7 @@ import Card from "../card/Card";
 
 const Activity = () => {
   return (
-    <div className="w-full flex flex-wrap flex-col md:flex-row items-center gap-5 justify-center pt-20">
+    <div className="w-full flex flex-wrap flex-col md:flex-row items-center gap-10 justify-center pt-20">
       <Card img="/smiley.png" text="Activité 1" />
       <Card img="/smiley.png" text="Activité 1" />
       <Card img="/smiley.png" text="Activité 1" />
