ded072f3da60a6adc6db5f0cecb994b3d6714216
Dayifour3 <123929396+Dayifour3@users.noreply.github.com>
2025-01-01 02:39:06 +0000

    Réajustement

--- Components/activity/Activity.tsx
+++ Components/activity/Activity.tsx
@@ -2,11 +2,18 @@ import Card from "../card/Card";
 
 const Activity = () => {
   return (
-    <div className="w-full flex flex-wrap flex-col md:flex-row items-center gap-10 justify-center pt-20">
-      <Card img="/smiley.png" text="Activité 1" />
-      <Card img="/smiley.png" text="Activité 1" />
-      <Card img="/smiley.png" text="Activité 1" />
-      <Card img="/smiley.png" text="Activité 1" />
+    <div className="flex flex-col gap-6 md:gap-10 pt-20">
+      <div className="w-full flex flex-wrap flex-col md:flex-row items-center gap-10 justify-center">
+        <Card img="/Group-18.png" text="IA/ANALYSE DE DONNEES" />
+        <Card img="/icon2.png" text="DEVELOPPEMENT WEB" />
+        <Card img="/icon-DATA.png" text="UI/UX DESIGN" />
+        <Card img="/Group-17.png" text="SECURITE" />
+      </div>
+      <div>
+        <h1 className="text-center text-4xl md:text-8xl text-btn">
+          Des formations numériques adaptées a vos besoins
+        </h1>
+      </div>
     </div>
   );
 };
