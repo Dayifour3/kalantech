f0bb219784e79139d6e7aa025bb9e1ab92ce2bf0
Dayifour3 <123929396+Dayifour3@users.noreply.github.com>
2025-01-02 17:36:20 +0000

    almost finished the project

--- Components/hero/Hero.tsx
+++ Components/hero/Hero.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col lg:flex-row items-center gap-5 h-[calc(100vh-100px)]">
+    <div className="mt-10 lg:mt-0 flex flex-col lg:flex-row items-center gap-5 md:h-[calc(100vh-100px)]">
       <div className="w-8/12 flex flex-col justify-center gap-8 lg:gap-20">
-        <h2 className="text-5xl xl:text-6xl font-bold">
+        <h2 className="pt-5 md:pt-0 text-3xl sm:text-4xl xl:text-6xl font-bold">
           KalanTech, une Edtech qui réinvente l’apprentissage grâce à
           L’intelligence artificielle
         </h2>
@@ -15,7 +15,7 @@ const Hero = () => {
           <button className="text-btn text-xl">Discussion</button>
         </div>
       </div>
-      <div className="relative w-[500px] h-[500px]">
+      <div className="w-[400px] h-[400px] relative md:w-[500px] md:h-[500px]">
         <Image src="/smiley.png" alt="" fill />
       </div>
     </div>
